2e3d09864620347fd41504a3f18ad7ddc85fd472
Damian X <dx4336969@gmail.com>
2024-06-16 06:10:05 +0000

    implement interfaces

---
+++ InterfacesDemo.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace InterfacesDemo
+{
+    interface IMovable
+    {
+        void Move();
+    }
+
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            Bicycle bicycle = new Bicycle();
+
+            Console.WriteLine("Interfaces:");
+            car.Move(); // Output: Car is moving
+            bicycle.Move(); // Output: Bicycle is moving
+        }
+    }
+}
