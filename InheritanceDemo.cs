23bc0572ecc99475e8d7467b563898c674d6263c
Damian X <dx4336969@gmail.com>
2024-06-16 06:08:35 +0000

    implement inheritance and method overriding

---
+++ InheritanceDemo.cs
@@ -0,0 +1,43 @@
+using System;
+
+namespace InheritanceDemo
+{
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal genericAnimal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            Console.WriteLine("Inheritance and Method Overriding:");
+            genericAnimal.MakeSound(); // Output: Some generic sound
+            dog.MakeSound(); // Output: Bark
+            cat.MakeSound(); // Output: Meow
+        }
+    }
+}
